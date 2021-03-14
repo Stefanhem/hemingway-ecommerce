6adce370b0acf603f04125df3c0b93b04074f911
Stefanhem <69218219+Stefanhem@users.noreply.github.com>
2021-03-14 12:11:33 +0100

    Update placanje.blade.php
    promene

--- resources/views/pages/footer/placanje.blade.php
+++ resources/views/pages/footer/placanje.blade.php
@@ -9,17 +9,16 @@
         <p class="paragraph-222 legal-paragraph" style="width: 100%">
             <strong>1. POUZEĆE</strong><br/>
             Nakon potvrde narudžbine roba će Vam biti poslata kurirskom službom, a Vi ćete je platiti kuriru pri
-            preuzimanju. Za narudžbine vrednosti veće od 4.000,00 dinara ili u slučaju posebne napomene kod pojedinih
+            preuzimanju. Za narudžbine vrednosti veće od 5.000,00 dinara ili u slučaju posebne napomene kod pojedinih
             proizvoda, Hemingway Vam pruža besplatnu isporuku robe na teritoriji cele Srbije. U svim ostalim slučajevima
-            Kupac plaća dostavu kuriru pri preuzimanju, prema važećem cenovniku kurirske službe.
+            Kupac plaća dostavu po fiksnoj ceni od 250 dinara.
         </p>
         <p class="paragraph-222 legal-paragraph" style="width: 100%">
             <strong>2. UPLATNICA ILI NALOG ZA PRENOS</strong><br/>
             Ovaj način plaćanja podrazumeva direktnu uplatu na naš račun putem uplatnice u bilo kojoj banci ili pošti, ili
             putem naloga za prenos ako ste korisnik elektronskog bankarstva (e-bankinga). Primer popunjene uplatnice
             ćete videti ukoliko izaberete ovaj način plaćanja prilikom kupovine. Roba se šalje na adresu isporuke po
-            evidentiranju uplate. NAPOMENA: Dostava se, ukoliko nije besplatna, plaća gotovinski kuriru prilikom isporuke.
-            Ukoliko plaćate kao pravno lice, kontaktirajte nas za instrukcije.
+            evidentiranju uplate. NAPOMENA: Dostava se, ukoliko nije besplatna, plaća takodje unapred zajedno sa naručenim proizvodima.
         </p>
         <p class="paragraph-222 legal-paragraph" style="width: 100%">
         Iz inostranstva trenutno je omogućena devizna uplata na račun, a u pripremi je još metoda plaćanja.
@@ -39,11 +38,10 @@
             cenu, datum izdavanja računa i specifikaciju prodajne cene, ali nije u obavezi da kuca fiskalni račun
             ako je dostava preko pošte ili kurirske službe.</li>
             <li>Sva naručena roba će biti poslata maksimalno do 5 radnih dana od potvrde narudžbine, <strong>a uobičajeni
-            rok za isporuku je 3 radna dana</strong>, ukoliko je plaćanje pouzećem. Za plaćanje na račun (uplatnica ili
+            rok za isporuku je 2 radna dana</strong>, ukoliko je plaćanje pouzećem. Za plaćanje na račun (uplatnica ili
             nalog za prenos), ovaj rok teče od momenta evidentiranja uplate.</li>
-            <li>Za sve narudžbine vrednosti veće od 4.000,00 dinara kao i za robu sa naznakom ‘’besplatna dostava’’
-            kupac ne plaća troškove dostave. U svim ostalim slučajevima dostavu plaća kupac, i to gotovinski
-            kuriru u trenutku isporuke.</li>
+            <li>Za sve narudžbine vrednosti veće od 5.000,00 dinara kao i za robu sa naznakom ‘’besplatna dostava’’
+            kupac ne plaća troškove dostave. U svim ostalim slučajevima dostavu plaća kupac, a fiksna cena dostave je 250 dinara.</li>
             <li>Prilikom preuzimanja pošiljke potrebno je da vizuelno pregledate paket, i ukoliko primetite da je
             transportna kutija eventualno značajno oštećena i posumnjate da je proizvod možda oštećen, imate
             pravo da odbijete prijem pošte o čemu je potrebno da nas što pre obavestite.</li>
